1651535bc99a67f412996fc066e05a9c5401322a
renatas7 <renatas.net@gmail.com>
2019-08-14 10:03:52 +0300

    Updates

--- precache-manifest.12b757d1ab0b6c9a4d7ea085ce2965bc.js
+++ precache-manifest.12b757d1ab0b6c9a4d7ea085ce2965bc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a32a0817bac9a72a49a67858c412010",
+    "revision": "1769a3669bb71627833e7c67ee79ecd9",
     "url": "/contactify-app/index.html"
   },
   {
-    "revision": "71d76cec0399e6b86dee",
+    "revision": "85b7c34fbb421843cc88",
     "url": "/contactify-app/static/css/main.72cb4922.chunk.css"
   },
   {
-    "revision": "11b79e353f06a43032f2",
-    "url": "/contactify-app/static/js/2.0eed1737.chunk.js"
+    "revision": "792c2e318db71c9743d8",
+    "url": "/contactify-app/static/js/2.f4a138e7.chunk.js"
   },
   {
-    "revision": "71d76cec0399e6b86dee",
-    "url": "/contactify-app/static/js/main.9aafdaa2.chunk.js"
+    "revision": "85b7c34fbb421843cc88",
+    "url": "/contactify-app/static/js/main.634c4694.chunk.js"
   },
   {
     "revision": "08928b3b8498ab6b5d7d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contactify-app/precache-manifest.ef39f81a3bb0e1971f1236cc43b91086.js"
+  "/contactify-app/precache-manifest.12b757d1ab0b6c9a4d7ea085ce2965bc.js"
 );
 
 self.addEventListener('message', (event) => {
