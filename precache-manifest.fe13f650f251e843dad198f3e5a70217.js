6b7bc8658ed26bb9cf9e3f09c049f76e89d07941
renatas7 <renatas.net@gmail.com>
2019-08-14 09:01:00 +0300

    Updates

---
+++ precache-manifest.fe13f650f251e843dad198f3e5a70217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f15f115da662e1fb9f0b120bf4fa89f",
+    "url": "/contactify-app/index.html"
+  },
+  {
+    "revision": "78041b7f7952e77f2a2c",
+    "url": "/contactify-app/static/css/main.72cb4922.chunk.css"
+  },
+  {
+    "revision": "11b79e353f06a43032f2",
+    "url": "/contactify-app/static/js/2.0eed1737.chunk.js"
+  },
+  {
+    "revision": "78041b7f7952e77f2a2c",
+    "url": "/contactify-app/static/js/main.01dd4142.chunk.js"
+  },
+  {
+    "revision": "08928b3b8498ab6b5d7d",
+    "url": "/contactify-app/static/js/runtime~main.4157fde2.js"
+  }
+]);
